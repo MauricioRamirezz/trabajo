d0a0aa45b66d4b481a0e3d65ccd07f85a045e2ca
C0nei <coneicode@gmail.com>
2026-05-17 20:18:14 -0500

    cambio inicial

---
+++ appteach copia/src/main/java/com/example/appteach/AppteachApplication.java
@@ -0,0 +1,13 @@
+package com.example.appteach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppteachApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppteachApplication.class, args);
+	}
+
+}
